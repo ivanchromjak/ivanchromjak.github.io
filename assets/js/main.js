f16fd99f26c4815e90a5c7939c42b4fc4d451592
Ivan Chromjak <ivan@guerilla.io>
2017-03-22 22:47:31 +0100

    logo and instagram gallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,42 @@
 /***************************************************
       As the page loads, cal these scripts
 ***************************************************/
+$(function(){
+
+  if($('#instafeed').length > 0){
+    var feed = new Instafeed({
+      //get: 'user',
+      //userId: '1045245980',
+      get: 'tagged',
+      tagName: 'awesome',
+      accessToken: '1045245980.ba4c844.e4e85ffbd253421ebf463634a9245454',
+      limit: 30,
+      resolution: 'standard_resolution',
+      template: '<div class="grid-item"><a href="{{link}}"><img class="img-fluid" src="{{image}}" /></a></div>',
+      after: function() {
+        // init Masonry
+        var $grid = $('.grid').masonry({
+          itemSelector: '.grid-item',
+          percentPosition: true,
+          columnWidth: '.grid-sizer',
+          gutter: 30
+        });
+        // layout Isotope after each image loads
+        $grid.imagesLoaded().progress( function() {
+          $grid.masonry();
+        });
+      }
+    });
+    feed.run();
+  }
+/*
+  $('.masonry').masonry({
+    // options...
+    itemSelector: '.item',
+    columnWidth: 200
+  });
+*/
+
+
+
+});
